5fe1db2b5f4c50013c78a1281bd48c33c69abaa9
axelburtz <axelburtz@gmail.com>
2023-01-24 20:43:41 +0100

    setup pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    redirect_to root_path, alert: 'Not authorized'
+  end
+
+  def ensure_admin
+    user_not_authorized unless current_user.admin?
+  end
 end
