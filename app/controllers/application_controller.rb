39197eaccca97249436d42dbd1cd2c12bbdfbccc
axelburtz <axelburtz@gmail.com>
2023-01-25 01:00:26 +0100

    add name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
 
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
 
   def ensure_admin
     user_not_authorized unless current_user.admin?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
